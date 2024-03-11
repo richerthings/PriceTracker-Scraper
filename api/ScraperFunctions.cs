f89ef3fa5853a637b5f96b667b4b6ae59cb067be
richerpaul <richer.paul@gmail.com>
2024-03-10 22:17:50 -0400

    fix: fixed for deployment

--- api/ScraperFunctions.cs
+++ api/ScraperFunctions.cs
@@ -1,22 +1,21 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace PriceTracker.Scraper
 {
-    public static class ScrapePricesForSubscriptions
+    public static class CheckPrices
     {
-        [FunctionName("ScrapePricesForSubscriptions")]
+        [FunctionName("CheckPrices")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log
-        )
+            ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
